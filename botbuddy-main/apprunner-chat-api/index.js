2c4164e67212921184505249e9bb93d02ba14a24
akhilkandari2501 <165754031+akhilkandari2501@users.noreply.github.com>
2025-01-22 14:43:42 +0530

    updated js

--- botbuddy-main/apprunner-chat-api/index.js
+++ botbuddy-main/apprunner-chat-api/index.js
@@ -1,52 +1,39 @@
-require('dotenv').config();
-const cors = require('cors');
-const express = require('express');
+const port = process.env.PORT || 3000,
+    http = require('http'),
+    fs = require('fs'),
+    html = fs.readFileSync('index.html');
 
-const {chat} = require('./components/chat-service');
-const { authenticateJWT } = require("./components/auth-service");
+const log = function(entry) {
+    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+};
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const server = http.createServer(function (req, res) {
+    if (req.method === 'POST') {
+        let body = '';
 
-// Use the cors middleware
-app.use(cors());
+        req.on('data', function(chunk) {
+            body += chunk;
+        });
 
-// Optionally, configure CORS to allow specific origins
-/* app.use(cors({
-    origin: ['https://chat.cloudevangelist.in/*', "http://localhost:3000/*"],
-    methods: ['OPTIONS', 'GET', 'POST'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-})); */
+        req.on('end', function() {
+            if (req.url === '/') {
+                log('Received a message.');
+            } else if (req.url = '/scheduled') {
+                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+            }
 
-// SSE endpoint
-app.get("/stream", authenticateJWT, async (req, res) => {
-  res.setHeader("Content-Type", "text/event-stream");
-  res.setHeader("Cache-Control", "no-cache");
-  res.setHeader("Connection", "keep-alive");
-
-  const chatMsg = req.query.chat;
-  const translationLanguage = req.query.translationLanguage;
-  const isSpeakerEnabled = req.query.isSpeakerEnabled;
-  if (!chatMsg) res.status(401).send("Exception: Chat Param is missing.");
-
-  const response = await chat(
-    chatMsg,
-    res.locals.decodedToken,
-    res,
-    isSpeakerEnabled,
-    translationLanguage
-  );
-
-  // Clean up when client closes connection
-  req.on("close", () => {
-    res.end();
-  });
+            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+            res.end();
+        });
+    } else {
+        res.writeHead(200);
+        res.write(html);
+        res.end();
+    }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+// Listen on port 3000, IP defaults to 127.0.0.1
+server.listen(port);
 
-
-
-//curl -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU3Mjg0ZGJhLWQzMzMtNDRlMC1iMjE2LWI0Y2M0ZjJkZDNjNCIsImN1c3RvbWVyIjoiUGFzc3BvcnQtU2V2YSIsIndlYnNpdGUiOiJodHRwczovL3d3dy5wYXNzcG9ydGluZGlhLmdvdi5pbi8iLCJhZGRpdGlvbmFsX3NpdGVzIjpbXSwiY2hhdGJvdG5hbWUiOiJQYXNzcG9ydCBTZXZhayIsImNoYXRib3RfbG9nb191cmwiOm51bGwsImluaXRpYWxfdGV4dCI6IkhlbGxvLCBJIGFtIFFDaGF0LiBIb3cgY2FuIEkgaGVscCB5b3U_IiwiZ3VhcmRyYWlscyI6IllvdSBhcmUgYSBwb2xpdGUgYW5kIGhvbmVzdCBjaGF0Ym90LCB3aG8gcmVzcG9uZHMgdG8gcXVlcmllcyB3aXRoIGVtcGF0aHkuIiwiYWNjZXB0VG5DIjp0cnVlLCJkb2NzIjpudWxsLCJib3Rfc3RhdHVzIjpudWxsLCJxY2hhdGZvcm1fc3RhdHVzIjoiQ29tcGxldGVkIiwicmVnaW9uUSI6Ik5PUlRIX1ZJUkdJTklBIiwiZXhwaXJ5X2RhdGV0aW1lIjpudWxsLCJyZXF1ZXN0ZXJfZW1haWwiOiJ0aXd2aWthQGFtYXpvbi5jb20iLCJhcHBsaWNhdGlvbklkUSI6IjU4NDU1MWY1LWVhOWUtNGQ2ZC04ODA1LTVlZDk0ZTAyZjc2ZSIsImluZGV4ZWRQYWdlcyI6IjkwMiIsImNyZWF0ZWRBdCI6IjIwMjQtMDgtMDJUMDQ6NTM6MTAuNjc3WiIsInVwZGF0ZWRBdCI6IjIwMjQtMDgtMDdUMTY6MjI6NTQuOTgzWiIsImlhdCI6MTcyMzYwODkyNCwiZXhwIjoxNzI0MjEzNzI0fQ.23IKc_qkn8f8JlGLa3K0UwM2jsLfWWVWkrzdHiVOLhk" "http://localhost:4000/stream?chat=%7B%22conversationId%22%3A%22%22%2C%22parentMessageId%22%3A%22%22%2C%22userMessage%22%3A%22hi%22%2C%22clientToken%22%3A%227f338202-97b3-4419-b434-833d6773ff5e%22%7D&stream=true&isSpeakerEnabled=false"
+// Put a friendly message on the terminal
+console.log('Server running at http://127.0.0.1:' + port + '/');
