66b82f4792f7553b47861d92764c5ed14ce00c98
sushilconverg <sushil.k@converg-in.com>
2025-04-14 18:19:14 +0530

    updated files qchat

--- amplify-chat-admin/app/projects/qchat/layout.tsx
+++ amplify-chat-admin/app/projects/qchat/layout.tsx
@@ -8,7 +8,7 @@ function Layout(
 ) {
     const { dispatch } = useUser();
     useEffect(() => {
-        dispatch({ type: "SETPROJECT", payload: { projectName: "Project BotBuddy" } })
+        dispatch({ type: "SETPROJECT", payload: { projectName: "ConvergIN-Bot" } })
     }, [])
     return (
         <>
