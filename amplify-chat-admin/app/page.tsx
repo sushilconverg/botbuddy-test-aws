f17ce0d3e719d065ce13cd4e120e0be39c62c85c
sushilconverg <sushil.k@converg-in.com>
2025-04-14 17:57:59 +0530

    Update page.tsx

--- amplify-chat-admin/app/page.tsx
+++ amplify-chat-admin/app/page.tsx
@@ -14,14 +14,14 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-4/5 md:px-20">
           <div className={styles.shape} />
            <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to BotBuddy Chatbot - Engage Smarter, Faster, and Better with BotBuddy.</strong>          
+            <strong>Welcome to ConvergIN-Bot Chatbot - Engage Smarter, Faster, and Better with ConvergIN-Bot.</strong>          
           </p>
 
           <Link
             href="/projects"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>BotBuddy</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>ConvergIN-Bot</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-1/5 md:px-28 md:py-12">
